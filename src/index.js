4cda4f37e62333286aaef5b5b320cfb0462d7a97
Astro Orbis <astroorbis@gmail.com>
2022-10-24 09:42:15 -0400

    v1.0.0 - Initial Push

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 const fs = require('fs')
+
+class BZDB {
+	construct() {
+		this.JSONObject = undefined || {};
+	}
+
+	add(key, value) {
+		this.JSONObject[key] = value;
+	}
+
+	get(key) {
+		return this.JSONObject[key];
+	}
+
+	remove(key) {
+		delete this.JSONObject[key];
+	}
+
+	dump(consolelog) {
+		if (consolelog){
+			console.log(this.JSONObject);
+		}
+		return this.JSONObject;
+	}
+
+	load(filename) {
+		this.JSONObject =  JSON.parse(fs.readFileSync(filename));
+	}
+
+	export() {
+		fs.writeFileSync('database.bzdb', JSON.stringify(this.JSONObject));
+	}
+}
+
+module.exports = BZDB
